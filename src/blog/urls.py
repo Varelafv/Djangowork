b80f333663d26bb99d8f5622284077d4f2d7850c
francisco.varela <francisco.varela@etu.umontpellier.fr>
2024-11-21 14:18:49 +0100

    new way to make the access different views using str and retourn that quand change depending on a dynamic variable .

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,12 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index , article_01,article_02,article_03
+from .views import index , article
 
 urlpatterns = [
     path('index/',index,name="blog-index"),
-    path('article-01/',article_01,name="blog-article01"),
-    path('article-02/', article_02, name="blog-article02"),
-    path('article-03/', article_03, name="blog-article03"),
+    path('article-<str:number_article>/',article,name="blog-article"),
 
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 def index (request ):
     return render(request,"blog/index.html")
 
-def article_01 (request ):
-    return render(request,"blog/article_01.html")
-def article_02 (request ):
-    return render(request,"blog/article_02.html")
-def article_03 (request ):
-    return render(request,"blog/article_03.html")
+def article(request,number_article):
+    if number_article in ["01","02","03"] :
+        return render(request,f"blog/article_{number_article}.html")
+    else :
+        return render(request, f"blog/article_not_found.html")
