2266d596bcb073b5e22b20bf060b45d23ce1b9f0
francisco.varela <francisco.varela@etu.umontpellier.fr>
2024-11-21 13:56:16 +0100

    In this session I added 3 news views the name article_N , it is the heavy way for to do that .It's working but its have another way for implement it .

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index , article_01,article_02,article_03
 
 urlpatterns = [
-    path('index/',index,name="blog-index")
+    path('index/',index,name="blog-index"),
+    path('article-01/',article_01,name="blog-article01"),
+    path('article-02/', article_02, name="blog-article02"),
+    path('article-03/', article_03, name="blog-article03"),
+
 ]
